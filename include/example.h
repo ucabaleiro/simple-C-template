4c5e83efd1ef7753072cb7131de8c6ac693cc475
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2021-09-02 22:28:55 -0300

    Added example

---
+++ include/example.h
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_H_
+#define EXAMPLE_H_
+
+const int NUM = 5;
+
+#endif
---
+++ src/example.c
@@ -0,0 +1,9 @@
+#include "example.h"
+#include <stdio.h>
+
+void main(){
+    for(int i = 0; i < NUM; i++){
+        printf("Hello, world!\n");
+    }
+
+}
